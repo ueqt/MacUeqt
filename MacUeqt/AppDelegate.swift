254c24f3e4bb38f78e1f70b50bdde058b1b0a6fd
ueqt <ueqtxu@gmail.com>
2018-09-30 14:58:13 +0800

    Initial Commit

---
+++ MacUeqt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacUeqt
+//
+//  Created by ueqt on 2018/9/30.
+//  Copyright © 2018 Ueqt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
