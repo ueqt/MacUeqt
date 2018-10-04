c73b1dc52dbce88a02f811ac2ada3582cad4dfb8
ueqt <ueqtxu@gmail.com>
2018-10-04 20:37:53 +0800

    add finder sync cannot run version

--- FinderSyncExtension/ExtensionWorker.swift
+++ FinderSyncExtension/ExtensionWorker.swift
@@ -14,38 +14,38 @@ struct ExtensionWorker {
     init(path: String) {
         self.path = path
     }
+    
+    func finderCurrentPath() -> String? {
+        guard let scriptUrl = Bundle.main.url(forResource: "getFinderCurrentPath", withExtension: "scpt"),
+            let scriptContent = try? String(contentsOf: scriptUrl) else {
+                return nil
+        }
+        
+        let script = NSAppleScript(source: scriptContent)
+        return script?.executeAndReturnError(nil).stringValue
+    }
 
     func run() {
-        guard let scriptUrl = Bundle.main.url(forResource: "iTerm", withExtension: "scpt") else {
-            return
-        }
-
-        guard let script = try? NSUserAppleScriptTask(url: scriptUrl) else {
-            return
-        }
-
-        script.execute(completionHandler: nil)
-
-//        let script = NSAppleScript(source: scriptContent)
-//        guard let path = script?.executeAndReturnError(nil).stringValue else {
+//        guard let scriptUrl = Bundle.main.url(forResource: "iTerm", withExtension: "scpt") else {
 //            return
 //        }
+//
+//        guard let script = try? NSUserAppleScriptTask(url: scriptUrl) else {
+//            return
+//        }
+//
+//        script.execute(completionHandler: nil)
 
-//        let process = Process()
-//        process.launchPath = "/Applications/iTerm.app/Contents/MacOS/iTerm2"
-////        process.arguments = ["\(self.path)"]
-////        process.launchPath = "/usr/bin/open"
-////        process.arguments = ["-a", "iTerm", "\(self.path)"]
-////        process.launchPath = "/usr/local/bin/code"
-////        process.arguments = ["."]
-//
-////        process.launchPath = "echo"
-////        process.arguments = ["hello"]
-////
-////        process.standardOutput = NSLog
-//
-//        process.launch()
-//        process.waitUntilExit()
+//        guard let path = finderCurrentPath() else {
+//            return
+//        }
+        
+        let process = Process()
+        process.launchPath = "/usr/bin/open"
+        process.arguments = ["-a", "iTerm", "\(self.path)"]
+        
+        process.launch()
+        process.waitUntilExit()
     }
 
 }
